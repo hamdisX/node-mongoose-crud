1e1eb3ca29d21f247fbb6573af61f4d9dc94c598
hamdimiled <hamdismiled@gmail.com>
2018-04-14 19:29:15 +0200

    refactoring controllers

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,7 +1,7 @@
 const Event = require('../models/event');
 
 
-module.exports = {
+/* module.exports = {
 
     //show all events 
     showEvents: (req, res) => {
@@ -26,7 +26,7 @@ module.exports = {
     },
 
 
-    // ***************crud mongoo*************************//
+      **************crud mongoo************************* 
 
     //seed our database
     seedEvents:(req,res)=>{
@@ -53,3 +53,60 @@ module.exports = {
 
 };
 
+ */
+
+
+ module.exports = {
+     showEvents : showEvents ,
+     showSingle : showSingle ,
+     seedEvents : seedEvents
+ }
+
+ /**
+  * show all events
+  */
+
+  function showEvents(req,res){
+      //get all events
+      Event.find({}, (err, events) => {
+        if (err) {
+          res.status(404);
+          res.send('Events not found!');
+    }
+
+      //return a view with data
+      res.render('pages/events', {events:events});
+  })
+}
+
+  /**
+   * show a single event
+   */
+
+   function showSingle(req,res){
+       //get a single event
+       const event = { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.' };
+       res.render('pages/single', { event: event });
+       }
+
+       function seedEvents(req, res) {
+        // create some events
+        const events = [
+          { name: 'Basketball', description: 'Throwing into a basket.' },
+          { name: 'Swimming', description: 'Michael Phelps is the fast fish.' },
+          { name: 'Weightlifting', description: 'Lifting heavy things up' },
+          { name: 'Ping Pong', description: 'Super fast paddles' }
+        ];
+      
+        // use the Event model to insert/save
+        Event.remove({}, () => {
+          for (event of events) {
+            var newEvent = new Event(event);
+            newEvent.save();
+          }
+        });
+      
+        // seeded!
+        res.send('Database seeded!');
+      }
+      
